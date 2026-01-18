ae9a38f023e789600d5d4d90efd21ade950ad3f9
phoenix-da-phenom <ehinormichael@ymail.com>
2026-01-18 23:03:34 +0100

    Trading pair tab, Tab switching functionality implemented

--- src/Components/TradingPairtab.jsx
+++ src/Components/TradingPairtab.jsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon1 from "../assets/icon1.svg";
 import Icon2 from "../assets/icon2.svg";
 import Icon3 from "../assets/icon3.svg";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { BsDisplay } from "react-icons/bs";
 
 export default function TradingPairtab() {
+ const icons= [Icon1, Icon2, Icon3]
+const [activeIndex, setActiveIndex]= useState(0)
+
+  const handleSwitch= ()=>{
+
+
+  }
+
   return (
     <div className="trading-pair-tab">
-      <div className="active">
-        <img src={Icon1} />
-      </div>
+    
+      <div style={{ display:'flex', gap:'5px' }}className="icon-wrapper">
+        {icons.map((icon, index)=>(
+          <div key={index} onClick={()=>setActiveIndex(index)}
+          className={activeIndex=== index ? 'active': ""}
+          >
+            <img src={icon}/>
+          </div>
+        ))}
 
-      <div>
-        <img src={Icon2} />
-      </div>
 
-      <div>
-        <img src={Icon3} />
+
       </div>
+ 
 
       <div className="trading-pair-tab__dropdown">
         <div>10</div>
